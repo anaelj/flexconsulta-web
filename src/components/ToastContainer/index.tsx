ef80790b988796cee405f2fd1516ccfcd046871d
Anael <anaelj@gmail.com>
2020-08-04 23:16:45 -0400

    post inicial

---
+++ src/components/ToastContainer/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import  {Container} from './styles';
+import {ToastMessage} from '../../hooks/toast';
+import Toast from './Toast';
+import {useTransition} from 'react-spring';
+ 
+interface ToastContainerProps {
+    messages: ToastMessage[];
+}
+
+
+const ToastContainer: React.FC<ToastContainerProps> = ({messages}) => {
+  
+    const messagesWithTransitions = useTransition(
+        messages,
+        messages => messages.id, 
+        {
+            from: {right: '-120%', opacity: 0},
+            enter: {right: '0%' , opacity: 1},
+            leave: {right: '-120%' , opacity: 0}                
+        }
+    );
+
+     return (
+        <Container >
+            {messagesWithTransitions.map(({item, key, props})=>(
+                        <Toast key={key} style={props} message={item} />
+            
+            ))}
+        </Container>
+    );
+  }
+
+export default ToastContainer;
